fba720cf284fb48f166a899d5b070b40801bf0f8
b0gg4rd <boggard@hotmail.com>
2017-03-04 04:31:05 -0600

    feat(app): add spring hello world!

---
+++ spring-helloworld/README.md
@@ -0,0 +1,19 @@
+# Spring Hello World!
+
+The classic **Hello World!** for **Spring**.
+
+## Requirements
+
+- JDK 1.8
+- Apache Maven 3.x
+
+## Building
+
+The first time just use:
+
+`$ mvn -N io.takari:maven:wrapper`
+
+For develop cycle use:
+
+`$ ./mvnw clean spring-boot:run`
+
---
+++ spring-helloworld/src/main/java/net/coatli/java/Application.java
@@ -0,0 +1,21 @@
+package net.coatli.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+	public static void main(final String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@GetMapping("/")
+	public String index() {
+	  return "Hello World!";
+	}
+
+}
