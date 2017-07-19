b6be351f432e3d24e2dc78621c9c1645a7baa55a
b0gg4rd <boggard@hotmail.com>
2017-07-19 01:48:10 -0500

    style(app): add space

--- sparkjava-helloworld/src/main/java/net/coatli/java/Application.java
+++ sparkjava-helloworld/src/main/java/net/coatli/java/Application.java
@@ -5,7 +5,9 @@ import static spark.Spark.get;
 public class Application {
 
   public static void main(final String[] args) {
-    get("/", (req, res) -> "Hello World!");
+
+	get("/", (req, res) -> "Hello World!");
+  
   }
 }
 
