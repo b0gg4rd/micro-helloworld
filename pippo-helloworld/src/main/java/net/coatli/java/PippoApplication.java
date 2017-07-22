7b46b56a291e3342ae1cfbbb3c28cff47b86179d
b0gg4rd <boggard@hotmail.com>
2017-07-22 08:36:59 -0500

    refactor(pippo): change app bootstrap

--- pippo-helloworld/src/main/java/net/coatli/java/PippoApplication.java
+++ pippo-helloworld/src/main/java/net/coatli/java/PippoApplication.java
@@ -3,19 +3,18 @@ package net.coatli.java;
 import ro.pippo.core.Application;
 import ro.pippo.core.Pippo;
 
-public class PippoApplication {
+public class PippoApplication extends Application {
 
   public static void main(String[] args) {
+    
+    new Pippo(new PippoApplication()).start();;
+  
+  }
+  
+  @Override
+  protected void onInit() {
 
-    new Pippo(new Application() {
-
-      @Override
-      protected void onInit() {
-
-        GET("/", (routeContext) -> routeContext.send("Hello World!"));
-
-      }
-    }).start();;
+    GET("/", (routeContext) -> routeContext.send("Hello World!"));
 
   }
 
