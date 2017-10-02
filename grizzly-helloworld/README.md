39895e18b69c5c0510952b2cceca3542e577bb77
b0gg4rd <boggard@hotmail.com>
2017-10-02 18:43:14 -0500

    feat(app): add grizzly hello world

---
+++ grizzly-helloworld/README.md
@@ -0,0 +1,14 @@
+# Grizzly - Hello World!
+
+The classic **Hello World!** for **Grizzly**.
+
+## Requirements
+
+- JDK 1.8
+
+## Building
+
+For execute use:
+
+`$ ./mvnw clean package exec:exec`
+
---
+++ grizzly-helloworld/src/main/java/net/coatli/java/microservice/GrizzlyHelloWorldApplication.java
@@ -0,0 +1,29 @@
+package net.coatli.java.microservice;
+
+import java.io.IOException;
+
+import org.glassfish.grizzly.http.server.HttpHandler;
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.grizzly.http.server.Request;
+import org.glassfish.grizzly.http.server.Response;
+
+public class GrizzlyHelloWorldApplication {
+
+  public static void main(final String[] args) throws IOException {
+
+    final HttpServer server = HttpServer.createSimpleServer();
+    server.getServerConfiguration().addHttpHandler(
+        new HttpHandler() {
+            @Override
+            public void service(final Request request, final Response response) throws Exception {
+                response.setContentType("text/plain");
+                //response.setContentLength(date.length());
+                response.getWriter().write("Hello World!");
+            }
+        }, "/");
+    server.start();
+    System.out.println("Press any key to stop the server...");
+    System.in.read();
+  }
+
+}
