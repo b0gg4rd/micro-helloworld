76946991384fe5382a271cdd266ec91c3b8599ee
b0gg4rd <boggard@hotmail.com>
2017-11-27 23:14:41 -0600

    refactor(logger): setup slf4j-simple

--- undertow-helloworld/src/main/java/net/coatli/java/microhelloworld/UndertowHelloWorldApplication.java
+++ undertow-helloworld/src/main/java/net/coatli/java/microhelloworld/UndertowHelloWorldApplication.java
@@ -12,12 +12,14 @@ public class UndertowHelloWorldApplication {
 
   public static void main(final String[] args) {
 
+    System.setProperty("org.jboss.logging.provider", "slf4j");
+
     Undertow.builder()
       .addHttpListener(8080, "0.0.0.0")
       .setBufferSize(1024 * 16)
-      .setIoThreads(Runtime.getRuntime().availableProcessors() * 2) //this seems slightly faster in some configurations
+      .setIoThreads(Runtime.getRuntime().availableProcessors() * 2)
       .setSocketOption(Options.BACKLOG, 10000)
-      .setServerOption(UndertowOptions.ALWAYS_SET_KEEP_ALIVE, false) //don't send a keep-alive header for HTTP/1.1 requests, as it is not required
+      .setServerOption(UndertowOptions.ALWAYS_SET_KEEP_ALIVE, false)
       .setServerOption(UndertowOptions.ALWAYS_SET_DATE, true)
       .setServerOption(UndertowOptions.RECORD_REQUEST_START_TIME, false)
       .setWorkerThreads(200)
